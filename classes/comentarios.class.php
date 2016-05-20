17df08cbb9b99b6ef1bb936d5111306daa91f83e
Jeferson Eurides <jeferson_eurides@hotmail.com>
2016-05-20 18:13:54 -0300

    Update Comentarios Class
    Classe comentario fazendo inserção e update de registros

--- classes/comentarios.class.php
+++ classes/comentarios.class.php
@@ -3,6 +3,10 @@
 include 'database.class.php';
 
 class Comentarios{
+	private $idComentario = "";
+	public $idNoticia = "";
+	public $comentario = "";
+	public $email = "";
 
 	public function getAll(){
 		$banco = new Database();
@@ -20,6 +24,38 @@ class Comentarios{
 
 		return $resultado = $consulta->fetch();	
 	}
+
+	public function save(){
+		$banco = new Database();
+		$consulta = $banco->db->prepare("INSERT INTO comentarios (id_noticia, comentario, email) VALUES (?,?,?)");
+		$consulta->execute(array($this->idNoticia, $this->comentario, $this->email));
+
+		return $resultado = $consulta->fetch();	
+	}
+
+	public function get($id){
+		$banco = new Database();
+		$consulta = $banco->db->prepare("SELECT * FROM comentarios WHERE id_comentario=?");
+		$consulta->execute(array($id));
+		$resultado = $consulta->fetch();	
+
+		$this->idComentario = $resultado['id_comentario'];
+		$this->idNoticia = $resultado['id_noticia'];
+		$this->comentario = $resultado['comentario'];
+		$this->email = $resultado['email'];
+	}
+
+	public function update(){
+		$banco = new Database();
+		$consulta = $banco->db->prepare("UPDATE comentarios SET id_noticia=?, comentario=?, email=? WHERE id_comentario=?");
+		$consulta->execute(array($this->idNoticia, $this->comentario, $this->email, $this->idComentario));
+
+		return $resultado = $consulta->fetch();	
+	}
+
+
+
+
 }
 
 ?>

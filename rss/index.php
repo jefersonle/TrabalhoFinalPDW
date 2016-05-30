7acd56afcf4176cfe0dfa54247a9e10b7badc938
Jeferson Eurides <jeferson_eurides@hotmail.com>
2016-05-30 20:12:31 -0300

    Rss terminado

--- rss/index.php
+++ rss/index.php
@@ -1,4 +1,5 @@
 <?php
+include "../classes/noticia.class.php";
 
 // Intanciamos/chamamos a classe
 $rss = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss></rss>');
@@ -6,23 +7,33 @@ $rss->addAttribute('version', '2.0');
 // Cria o elemento <channel> dentro de <rss>
 $canal = $rss->addChild('channel');
 // Adiciona sub-elementos ao elemento <channel>
-$canal->addChild('title', 'Meu primeiro RSS');
-$canal->addChild('link', 'http://www.meusite.com/');
-$canal->addChild('description', 'Este é o meu primeiro RSS, uha!');
-// Cria um elemento <item> dentro de <channel>
-$item = $canal->addChild('item');
-// Adiciona sub-elementos ao elemento <item>
-$item->addChild('title', 'Meu segundo artigo');
-$item->addChild('link', 'http://www.meusite.com/artigos.php?id=2');
-$item->addChild('description', 'Esse é um resumo do meu segundo artigo.');
+$canal->addChild('title', 'PG News - RSS');
+$canal->addChild('link', 'http://www.pgnews.com/');
+$canal->addChild('description', 'Últimas 10 notícias via rss!');
 
-// Cria outro elemento <item> dentro de <channel>
-$item = $canal->addChild('item');
-// Adiciona sub-elementos ao elemento <item>
-$item->addChild('title', 'Meu primeiro artigo');
-$item->addChild('link', 'http://www.meusite.com/artigos.php?id=1');
-$item->addChild('description', 'Esse é um resumo do meu primeiro artigo.');
-$item->addChild('pubDate', date('r'));
+$noticia = new Noticia();
+$noticias = $noticia->getAll();
+$cont = 1;
+foreach ($noticias as $noticia) {
+	if($cont <=10){
+		// Cria  elemento <item> dentro de <channel>
+		$titulo = $noticia['titulo'];
+		$link = $noticia['link'];
+		$gravata = $noticia['gravata'];
+		$data = $noticia['data'];
+
+		$item = $canal->addChild('item');
+		// Adiciona sub-elementos ao elemento <item>
+		$item->addChild('title', $titulo);
+		$item->addChild('link', $link);
+		$item->addChild('description', $gravata);
+		$item->addChild('pubDate', $data);
+	}
+	$cont++;
+}
+
+
+
 // Define o tipo de conteúdo e o charset
 header("content-type: application/rss+xml; charset=utf-8");
 
